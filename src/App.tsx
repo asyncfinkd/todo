d03bb92fe6a0fee458dca7cd5a8900ef9d33ea73
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-08-26 14:58:37 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TextField from "./components/TextField";
+import Swal from 'sweetalert2';
+import { useEffect } from 'react';
 
 interface TextNode {
   text: string
@@ -8,12 +10,40 @@ interface TextNode {
 const App: React.FC = () => {
   const [value, setValue] = useState<TextNode>({ text: "Write Something..." });
   const [input, setInput] = useState<string>("");
+  const [data, setData] = useState<any>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const addItem = () => {
+    if (!input) {
+      inputRef.current?.focus();
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please enter a something...",
+      });
+    } else {
+      data.push({
+        text: input,
+        id: data.length + 1
+      });
+    }
+  }
 
   return (
     <>
       <div className="container flex:direction">
         <h2>ToDo Application</h2>
-        <TextField text={value.text} input={input} handleChange={e => setInput(e.target.value)} />
+        <TextField text={value.text} input={input} handleChange={e => setInput(e.target.value)} refValue={inputRef} />
+        <button type="button" className="btn btn-primary" style={{ width: "300px" }} onClick={addItem}>Add to List</button>
+        {data.map((item?: any) => {
+          return (
+            <>
+              <div key={item.id}>
+                <p>{item.text}</p>
+              </div>
+            </>
+          )
+        })}
       </div>
     </>
   );
