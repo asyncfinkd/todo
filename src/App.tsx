3973d450e13473b4dffec13e61ecf12e538eb6a5
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-26 22:03:57 +0400

    :rocket: re-install ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,25 @@
-import React, { useState, useRef } from 'react';
-import TextField from "./components/TextField";
-import Swal from 'sweetalert2';
-import { useEffect } from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-interface TextNode {
-  text: string
-}
-
-const App: React.FC = () => {
-  const [value, setValue] = useState<TextNode>({ text: "Write Something..." });
-  const [input, setInput] = useState<string>("");
-  const [data, setData] = useState<any>([]);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const addItem = () => {
-    if (!input) {
-      inputRef.current?.focus();
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Please enter a something...",
-      });
-    } else {
-      data.push({
-        text: input,
-        id: data.length + 1
-      });
-    }
-  }
-
+function App() {
   return (
-    <>
-      <div className="container flex:direction">
-        <h2>ToDo Application</h2>
-        <TextField text={value.text} input={input} handleChange={e => setInput(e.target.value)} refValue={inputRef} />
-        <button type="button" className="btn btn-primary" style={{ width: "300px" }} onClick={addItem}>Add to List</button>
-        {data.map((item?: any) => {
-          return (
-            <>
-              <div key={item.id}>
-                <p>{item.text}</p>
-              </div>
-            </>
-          )
-        })}
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
